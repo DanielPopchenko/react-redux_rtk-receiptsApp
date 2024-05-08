811c0a233645b9c0e95279210d8043704a96b43b
Daniel Popchenko <104201822+DanielPopchenko@users.noreply.github.com>
2024-05-08 16:10:41 +0300

    .

--- src/store/api/api.js
+++ src/store/api/api.js
@@ -1,23 +1,22 @@
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
-const API_URL = "http://localhost:4200/recipes";
+const API_URL = 'http://localhost:8000/recipes';
 
 export const api = createApi({
-	reducerPath: "api",
-	tagTypes: ["Recipe"],
-	baseQuery: fetchBaseQuery({
-		baseUrl: API_URL,
-	}),
-	endpoints: (builder) => ({
-		getRecipes: builder.query({
-			query: () => "/?_sort=id&_order=desc",
-			providesTags: () => [
-				{
-					type: "Recipe",
-				},
-			],
-		}),
-	}),
+    reducerPath: 'api',
+    tagTypes: ['Recipe'],
+    baseQuery: fetchBaseQuery({
+        baseUrl: API_URL,
+    }),
+    endpoints: builder => ({
+        getRecipes: builder.query({
+            // sorting by the order
+            query: () => '/?_sort=id&_order=desc',
+            providesTags: () => [{
+                type: 'Recipe',
+            }, ],
+        }),
+    }),
 });
 
 export const { useGetRecipesQuery } = api;
